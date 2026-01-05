36778eaac37ace1acc2726ff8cbf1a7f634786ca
Soroushbiro84 <166137295+Soroushbiro84@users.noreply.github.com>
2026-01-05 17:36:53 +0330

    Almost Done! :)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -116,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tehran'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
